866dd774d41f1713f7524c0fdb14c9b7183e5f4f
Eldar Gabdullin <eldargab@gmail.com>
2014-08-01 20:29:55 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#async.promise
+
+Non-blocking promise protocol and implementation.
+
+##License
+
+Copyright © 2014 Eldar Gabdullin
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dar/async.promise "0.1.0"
+  :description "Non-blocking promise protocol and implementation"
+  :url "http://github.com/dar-clojure/async.promise"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
